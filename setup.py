94433b7f026be67f68a52e7a14b5254b6a56bb64
kiki7000 <devkiki7000@gmail.com>
2020-09-06 21:02:18 +0900

    Added Long Description for pypi

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ import setuptools
 
 setuptools.setup(
     name = 'kiki_sort',
-    version = '1.0.1',
+    version = '1.0.2',
     description = 'Sort Functions',
+    long_description = open('README.md', 'r', encoding = 'utf-8').read(),
+    long_description_content_type = 'text/markdown',
     license = 'GNUv3',
     author = 'kiki7000',
     url = 'https://github.com/kiki7000/sorting_algorithm',
